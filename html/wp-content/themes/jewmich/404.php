0f84830f495275bbd55c3ac411c1c2f5ead4de35
Mason Malone <mason@masonm.org>
2017-02-11 20:13:04 -0800

    Add 404 page

---
+++ html/wp-content/themes/jewmich/404.php
@@ -0,0 +1,40 @@
+<?php
+get_header();
+?>
+
+<table border="0" id="table1" width="475">
+ <tbody>
+  <tr>
+   <td valign="top">
+    <p align="center">
+     <span class="chabad">
+      <span class="chabad-header">
+       Sorry!
+       <br/>
+       The page you have requested could not be found.
+       <br/>
+       Unfortunately, the page you attempted to access could not be found.
+       <br/>
+       <br/>
+       Either the address you entered is incorrect or it is outdated.
+       <br/>
+       Please check the address and try again.
+       <br/>
+       If you need further assistance or are looking
+       <br/>
+       for a particular page,
+       <a href="contact">
+        please let us know
+       </a>
+       .
+       <br/>
+      </span>
+     </span>
+    </p>
+   </td>
+  </tr>
+ </tbody>
+</table>
+<?php
+get_footer();
+?>
