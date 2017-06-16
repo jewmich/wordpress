e2fb6c12f8e0324c0c4c7fcb02748e4ee2d7f20f
Mason Malone <mason@masonm.org>
2017-06-15 19:38:20 -0700

    Allow file editing in admin since everyone has Google Authenticator enabled

--- html/wp-config.php
+++ html/wp-config.php
@@ -33,9 +33,6 @@ require_once(__DIR__ . '/../secrets/secrets-' . ENVIRONMENT . '.php');
 // Force SSL for all logins and wp-admin access, except in development/testing
 define('FORCE_SSL_ADMIN', ENVIRONMENT === 'production');
 
-// Don't allow editing files in admin because it's too big of a security risk
-define('DISALLOW_FILE_EDIT', ENVIRONMENT !== 'development');
-
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
 
