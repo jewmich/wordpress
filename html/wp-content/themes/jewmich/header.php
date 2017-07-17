815620c57ec7db68f5f8e9b70e4048fac19a807e
Mason Malone <mason@masonm.org>
2017-07-16 21:52:39 -0700

    Horizontally center the navbar

--- html/wp-content/themes/jewmich/header.php
+++ html/wp-content/themes/jewmich/header.php
@@ -50,86 +50,84 @@
 		<?php endif ?>
 	</header>
 	<nav class="navbar navbar-inverse navbar-static-top" role="navigaton">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navBar">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-				</button>
-		   </div>
-			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse" id="navBar">
-				<ul class="nav navbar-nav">
-					<li class="dropdown">
-						<a class="dropdown-toggle" id="students" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">STUDENTS <span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="/studentcenter">About U of M Chabad</a></li>
-							<li><a href="/birthright">Free Trip to Israel</a></li>
-							<li><a href="/mythandfact">Myths & Facts</a></li>
-							<li><a href="/studycenter">Judaic Classes</a></li>
-							<li><a href="/shabbat">Shabbat Dinner</a></li>
-							<li><a href="/candlelighting">Candle Lighting Times</a></li>
-							<li><a href="/kosher">Kosher in Ann Arbor</a></li>
-							<li><a href="/map">Map</a></li>
-							<li><a href="/visiting&amp;living">Visiting & Living</a></li>
-							<li><a href="/payment">Make a Payment</a></li>
-						</ul>
-					</li>
-					<li class="dropdown">
-						<a class="dropdown-toggle" id="adults" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">COMMUNITY <span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="/cong">Congregation Chabad</a></li>
-							<li><a href="/studycenter">Adult Education</a></li>
-							<li><a href="http://www.mycampganisrael.com">Camp Gan Israel</a></li>
-							<li><a href="/chs">Chabad Hebrew School</a></li>
-							<li><a href="/mikvah">Mikvah</a></li>
-							<li><a href="/kiddush">Sponsor a Shabbat Lunch</a></li>
-							<li><a href="/jwc">Jewish Women's Circle</a></li>
-							<li><a href="/kosher">Kosher in Ann Arbor</a></li>
-							<li><a href="/visiting&amp;living">Visiting & Living</a></li>
-							<li><a href="/payment">Make a Payment</a></li>
-						</ul>
-					</li>
-					
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" id="events" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">EVENTS <span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="/highholiday">High Holiday Reservation</a></li>
-							<li><a href="/kiddush">Sponsor a Kiddush Lunch</a></li>
-							<li><a href="/shabbat">RSVP for a Student Shabbat Dinner</a></li>
-						</ul>
-					</li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" id="contact_us" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">CONTACT US <span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="/contact">Contact Information</a></li>
-							<li><a href="/map">Map</a></li>
-							<li><a href="/aboutus">About Us</a></li>
-							<li><a href="/visiting&amp;living">Visiting & Living</a></li>
-							<li><a href="/payment">Make a Payment</a></li>
-							<li><a href="/ask">Ask the Rabbi</a></li>
-							<li><a href="/candlelighting">Candle Lighting Times</a></li>
-							<li><a href="/map">Map</a></li>
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navBar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+			</button>
+		</div>
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="navBar">
+			<ul class="nav navbar-nav">
+				<li class="dropdown">
+					<a class="dropdown-toggle" id="students" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">STUDENTS <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="/studentcenter">About U of M Chabad</a></li>
+						<li><a href="/birthright">Free Trip to Israel</a></li>
+						<li><a href="/mythandfact">Myths & Facts</a></li>
+						<li><a href="/studycenter">Judaic Classes</a></li>
+						<li><a href="/shabbat">Shabbat Dinner</a></li>
+						<li><a href="/candlelighting">Candle Lighting Times</a></li>
+						<li><a href="/kosher">Kosher in Ann Arbor</a></li>
+						<li><a href="/map">Map</a></li>
+						<li><a href="/visiting&amp;living">Visiting & Living</a></li>
+						<li><a href="/payment">Make a Payment</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a class="dropdown-toggle" id="adults" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">COMMUNITY <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="/cong">Congregation Chabad</a></li>
+						<li><a href="/studycenter">Adult Education</a></li>
+						<li><a href="http://www.mycampganisrael.com">Camp Gan Israel</a></li>
+						<li><a href="/chs">Chabad Hebrew School</a></li>
+						<li><a href="/mikvah">Mikvah</a></li>
+						<li><a href="/kiddush">Sponsor a Shabbat Lunch</a></li>
+						<li><a href="/jwc">Jewish Women's Circle</a></li>
+						<li><a href="/kosher">Kosher in Ann Arbor</a></li>
+						<li><a href="/visiting&amp;living">Visiting & Living</a></li>
+						<li><a href="/payment">Make a Payment</a></li>
+					</ul>
+				</li>
+				
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" id="events" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">EVENTS <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="/highholiday">High Holiday Reservation</a></li>
+						<li><a href="/kiddush">Sponsor a Kiddush Lunch</a></li>
+						<li><a href="/shabbat">RSVP for a Student Shabbat Dinner</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" id="contact_us" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">CONTACT US <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="/contact">Contact Information</a></li>
+						<li><a href="/map">Map</a></li>
+						<li><a href="/aboutus">About Us</a></li>
+						<li><a href="/visiting&amp;living">Visiting & Living</a></li>
+						<li><a href="/payment">Make a Payment</a></li>
+						<li><a href="/ask">Ask the Rabbi</a></li>
+						<li><a href="/candlelighting">Candle Lighting Times</a></li>
+						<li><a href="/map">Map</a></li>
 
 
-						</ul>
-					</li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" id="donate" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">DONATE <span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="/donate">Donate Online</a></li>
-							<li><a href="/donation">About Donations</a></li>
-							<li><a href="/wishlist">Chabad Wish List</a></li>
-							<li><a href="/chaiclub">Chai Club</a></li>
-							<li><a href="/charitybox">Request a Charity Box</a></li>
-							<li><a href="/payment">Make a Payment</a></li>
+					</ul>
+				</li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" id="donate" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">DONATE <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="/donate">Donate Online</a></li>
+						<li><a href="/donation">About Donations</a></li>
+						<li><a href="/wishlist">Chabad Wish List</a></li>
+						<li><a href="/chaiclub">Chai Club</a></li>
+						<li><a href="/charitybox">Request a Charity Box</a></li>
+						<li><a href="/payment">Make a Payment</a></li>
 
-						</ul>
-					</li>
-				</ul>
-			</div><!-- /.navbar-collapse -->
-		</div><!-- /.container -->
+					</ul>
+				</li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
 	</nav>
 	<div class="container<?= get_post_meta(get_the_ID(), 'no_sidebar', true) ? '' : ' has-sidebar' ?>" id="pageContent">
 		<div class="row">
