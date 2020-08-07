3add566a5c6928172be0973022f929bc210a7264
Mason Malone <mason@masonm.org>
2020-08-06 21:37:13 -0700

    Update import_sql.sh for new server

--- scripts/import_sql.sh
+++ scripts/import_sql.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-ssh -C alterga2@jewmich.com 'mysqldump -u jewmichwordpress -h mysql.jewmich.com jewmichmainsql' > dump.sql
+set -e
+
+ssh -C jewmich_com@jewmich.com 'mysqldump -p -u jewmichwordpress -h mysql.jewmich.com jewmichmainsql' > dump.sql
 sed -i 's/DEFINER=[^*]*\*/\*/g' dump.sql
 echo 'UPDATE wp_options set option_value = "http://127.0.0.1" where option_value = "https://www.jewmich.com";' >> dump.sql
 echo 'UPDATE wp_usermeta set meta_value = "disabled" where meta_key = "googleauthenticator_enabled";' >> dump.sql
