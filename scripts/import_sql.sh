6bc14d4aa4fa08fb4c0e22fde96bb05886507d30
Mason Malone <651224+MasonM@users.noreply.github.com>
2022-12-12 21:53:08 -0800

    Update MySQL Docker image and import script

--- scripts/import_sql.sh
+++ scripts/import_sql.sh
@@ -5,5 +5,6 @@ set -ex
 ssh -C jewmich_com@jewmich.com 'mysqldump -p -u jewmichwordpress -h mysql.jewmich.com jewmichmainsql' > dump.sql
 sed -i 's/DEFINER=[^*]*\*/\*/g' dump.sql
 echo 'UPDATE wp_options set option_value = "http://127.0.0.1" where option_value = "https://www.jewmich.com";' >> dump.sql
+echo 'UPDATE wp_options set option_value = "a:0:{}" where option_name = "googleauthenticator_mandatory_mfa_roles";' >> dump.sql
 echo 'UPDATE wp_usermeta set meta_value = "disabled" where meta_key = "googleauthenticator_enabled";' >> dump.sql
 mysql -u root -pwordpress -h 127.0.0.1 jewmichmainsql < dump.sql
