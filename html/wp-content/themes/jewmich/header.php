f6082998d0f2c540069c947da879e92238d228f9
Mason Malone <651224+MasonM@users.noreply.github.com>
2025-01-12 19:26:57 -0800

    Remove recaptcha JS for performnace
    Signed-off-by: Mason Malone <651224+MasonM@users.noreply.github.com>

--- html/wp-content/themes/jewmich/header.php
+++ html/wp-content/themes/jewmich/header.php
@@ -12,7 +12,6 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
-<script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <body <?php body_class(); ?>>
 	<header style="background-image: url(<?= header_image() ?>)">
